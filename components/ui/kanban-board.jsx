80c445b435be44f082f076b1fed380e20134d837
Lisa <lisali.lyj@gmail.com>
2017-08-14 00:07:00 +0000

    change panel style

--- components/ui/kanban-board.jsx
+++ components/ui/kanban-board.jsx
@@ -1,27 +1,14 @@
 import React from 'react';
 
-import Panel from './panel';
-import AddCard from '../containers/add-card';
+import Lane from './lane';
 
 export default class KanbanBoard extends React.Component {
     render() {
         return(
-            <div className="container board-lane">
-                <AddCard />
-                <div className='row'>
-                    <Panel
-                        title = "To Do"
-                        filter = 'todo'                   
-                    />
-                    <Panel
-                        title = "In Progress"
-                        filter = 'in-progress'                 
-                    />
-                    <Panel
-                        title = "Done"
-                        filter =  'done'                   
-                    />
-                </div>
+            <div className = "container">
+                <Lane />
+                <Lane />
+                <Lane />
             </div>
         )
     }
